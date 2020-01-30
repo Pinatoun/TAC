f628c17e2a1db2f6934641b46aea2873c0a71f61
Slipkvoc <100348736@alumnos.uc3m.es>
2020-01-30 17:34:05 +0100

    Comentarios y mcd actualizado

--- src/ejerciciosIntroductorios/MCD.java
+++ src/ejerciciosIntroductorios/MCD.java
@@ -7,7 +7,7 @@ public class MCD {
 
 	public static void main(String[] args) {
 		BigInteger n1 = new BigInteger("30");
-		BigInteger n2 = new BigInteger("12");
+		BigInteger n2 = new BigInteger("60");
 		System.out.println(descomposicion(n1, n2));
 		
 	}
@@ -18,28 +18,34 @@ public class MCD {
 		BigInteger one = new BigInteger("1");
 		ArrayList<BigInteger> divisores1 = new ArrayList<BigInteger>(), divisores2 = new ArrayList<BigInteger>();
 		BigInteger mcd = new BigInteger("1");		
+		//Bucle donde entra mientras todavia puedan dividirse
 		while(!n1.equals(one) || !n2.equals(one)) {
+			//Si ninguno de los dos es divisible por el divisor, buscamos el siguiente numero primo
 			if(!zero.equals(n1.mod(divisor)) && !zero.equals(n2.mod(divisor))) {
 				divisor = divisor.nextProbablePrime();
 				continue;
 			}
+			//Si el primer numero es divisible entre el divisor
 			if(zero.equals(n1.mod(divisor))) {
 				n1 = n1.divide(divisor);
 				divisores1.add(divisor);
 				System.out.println("n1    "+divisor);
 			}
+			//Si el segundo numero es divisible entre el divisor
 			if(zero.equals(n2.mod(divisor))) {
 				n2 = n2.divide(divisor);
 				divisores2.add(divisor);
 				System.out.println("n2    "+divisor);
 			}
 		}
+		//Recorremos las dos listas de divisores
 		for(int i = 0, j  = 0; i < divisores1.size() && j < divisores2.size();) {
-			System.out.println(i+"       "+j);
+			//Si los dos divisores son iguales, multiplicamos el mcd por el divisor y pasamos al siguiente elemento de las listas
 			if(divisores1.get(i).equals(divisores2.get(j))) {
-				mcd.multiply(divisores1.get(i));
+				mcd = mcd.multiply(divisores1.get(i));
 				i++;
 				j++;
+			//Avanzamos en la lista que tenga el menor divisor
 			}else if(divisores1.get(i).compareTo(divisores2.get(j)) < 0) {
 				i++;
 			}else j++;
