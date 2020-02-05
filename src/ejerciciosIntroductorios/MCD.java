e660853233725179ee39a40fd16ba4eee1669a4e
GodNietzsche <36699393+GodNietzsche@users.noreply.github.com>
2020-02-05 14:15:51 +0100

    Inicio de analisis de complejidad

--- src/ejerciciosIntroductorios/MCD.java
+++ src/ejerciciosIntroductorios/MCD.java
@@ -64,17 +64,17 @@ public class MCD {
 
 	// Metodo de euclides para calcular el mcd
 	public static BigInteger euclides(BigInteger n1, BigInteger n2) {
-		BigInteger zero = new BigInteger("0"), divisor, dividendo, resto;
+		BigInteger zero = new BigInteger("0"), divisor, dividendo, resto; // 4
 		// Seleccionamos el mayor de los dos numeros como dividendo y el otro como divisor
-		if(n1.compareTo(n2) < 0) {
+		if(n1.compareTo(n2) < 0) { //2
 			divisor = n1;
 			dividendo = n2;
-		}else if(n1.compareTo(n2) > 0) {
-			divisor = n2;
-			dividendo = n1;
+		}else if(n1.compareTo(n2) > 0) { //2
+			divisor = n2; //1
+			dividendo = n1; //1
 		}else return n1; // Si son iguales devolvemos ese numero
 		// Mientras el resto sea distinto de cero no habremos encontrado el mcd
-		while(!zero.equals(dividendo.mod(divisor))) {
+		while(!zero.equals(dividendo.mod(divisor))) { //2+n(2+2+1+1)????
 			resto = dividendo.mod(divisor);
 			dividendo = divisor;
 			divisor = resto;
@@ -86,34 +86,34 @@ public class MCD {
 	public static BigInteger siguientePrimo(BigInteger n){
 		// Vamos sumando 1 al numero proporcionado hasta encontrar un numero primo
 		do{
-			n = n.add(new BigInteger("1"));
-		}while(!isPrimo2(n));
+			n = n.add(new BigInteger("1")); //1
+		}while(!isPrimo2(n)); // n(1+1) o n(1+p)??
 		return n;
 	}
 
-	// Metodo que determina si un numero es primo con pasos de 1
+	// Metodo que determina si un numero es primo con pasos de 1 | T(n) = 3+1+3+n(2+3+2) = 7+7n
 	public static boolean isPrimo1(BigInteger n) {
-		BigInteger zero = new BigInteger("0"), one = new BigInteger("1"), two = new BigInteger("2");
+		BigInteger zero = new BigInteger("0"), one = new BigInteger("1"), two = new BigInteger("2"); //3
 		// Comprobamos si es divisible por algun numero desde 2 hasta n/2
-		for (BigInteger i = new BigInteger("2"); n.divide(two).compareTo(i) > 0; i = i.add(one)) {
+		for (BigInteger i = new BigInteger("2"); n.divide(two).compareTo(i) > 0; i = i.add(one)) { //1+3+n(2+3+2)
 			if (n.mod(i).equals(zero)) {
 				return false;
 			}
 		}
-		return true;
+		return true; //1
 	}
 
-	// Metodo que determina si un numero es primo con pasos de 2
+	// Metodo que determina si un numero es primo con pasos de 2 T(n) = 2+2+1+2+(n/2)(2+3+2)+1 = 8+7n/2
 	public static boolean isPrimo2(BigInteger n) {
-		BigInteger zero = new BigInteger("0"), two = new BigInteger("2");
+		BigInteger zero = new BigInteger("0"), two = new BigInteger("2"); //2
 		// Si es divisible por dos no es primo
-		if(n.divide(two).equals(zero)) return false;
+		if(n.divide(two).equals(zero)) return false; //2
 		// Comprobamos si es divisible por algun numero impar desde 3 hasta n/2
-		for (BigInteger i = new BigInteger("3"); n.divide(two).compareTo(i) > 0; i = i.add(two)) {
+		for (BigInteger i = new BigInteger("3"); n.divide(two).compareTo(i) > 0; i = i.add(two)) { //1+3+(n/2)(2+3+2)
 			if (n.mod(i).equals(zero)) {
 				return false;
 			}
 		}
-		return true;
+		return true; //1
 	}
 }
