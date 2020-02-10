57fd789e0943b2f8bb32e81550bc0bc192ab0ee8
GodNietzsche <36699393+GodNietzsche@users.noreply.github.com>
2020-02-10 01:11:47 +0100

    corrección

--- src/ejerciciosIntroductorios/MCD.java
+++ src/ejerciciosIntroductorios/MCD.java
@@ -18,14 +18,14 @@ public class MCD {
 		System.out.println("Execution time for the descomposition algorithm (miliseconds): "+((time2-time1)/1000000));
 	}
 
-	// T(n) = 6+4+2+3+5+7n+5n+15n^2+(7/2)n^3-5-15n-(7/2)n^2+10+1+1+5+3+3+3+1+1+1 = 44-3n+(23/2)n^2+(7/2)n^3
+	// T(n) = 6+4+2+3+5+7n+5n+15n^2+(7/4)n^3-5-15n-(7/4)n^2+10+1+1+5+3+3+3+1+1+1 = 44-3n+(53/2)n^2+(7/4)n^3
 	public static BigInteger descomposicion(BigInteger n1, BigInteger n2) {
 		BigInteger divisor = new BigInteger("2"), zero = new BigInteger("0"), one = new BigInteger("1"); //6
 		ArrayList<BigInteger> divisores1 = new ArrayList<BigInteger>(), divisores2 = new ArrayList<BigInteger>(); //4
 		BigInteger mcd = new BigInteger("1"); //2
 		if(n1.equals(zero ) || n2.equals(zero)) return zero; //3
 		// Bucle donde entra mientras todavia puedan dividirse
-		while (!n1.equals(one) || !n2.equals(one)) { //5+7n+(n-1)(1+1+3+15n+(7/2)n^2)+2+2+1+2+2+1 (peor caso, ambos son el mismo numero primo)
+		while (!n1.equals(one) || !n2.equals(one)) { //5+7n+(n-1)(1+1+3+15n+(7/4)n^2)+2+2+1+2+2+1 (peor caso, ambos son el mismo numero primo)
 			// Si ninguno de los dos es divisible por el divisor, buscamos el siguiente
 			// numero primo
 			if (!zero.equals(n1.mod(divisor)) && !zero.equals(n2.mod(divisor))) {
@@ -82,12 +82,12 @@ public class MCD {
 		return divisor; //1
 	}
 	
-	// Metodo que determina cual es el siguiente numero primo al proporcionado |T(n) = 2+n(2+12+7n/2+1)+1 = 3+15n+(7/2)n^2
+	// Metodo que determina cual es el siguiente numero primo al proporcionado |T(n) = 2+n(2+12+7n/4+1)+1 = 3+15n+(7/4)n^2
 	public static BigInteger siguientePrimo(BigInteger n){
 		// Vamos sumando 1 al numero proporcionado hasta encontrar un numero primo
 		do{
 			n = n.add(new BigInteger("1")); //2
-		}while(!isPrimo2(n)); // n(2+12+7n/2+1)
+		}while(!isPrimo2(n)); // n(2+12+7n/4+1)
 		return n; //1
 	}
 
@@ -95,7 +95,7 @@ public class MCD {
 	public static boolean isPrimo1(BigInteger n) {
 		BigInteger zero = new BigInteger("0"), one = new BigInteger("1"), two = new BigInteger("2"); //6
 		// Comprobamos si es divisible por algun numero desde 2 hasta n
-		for (BigInteger i = new BigInteger("2"); n.divide(two).compareTo(i) > 0; i = i.add(one)) { //2+3+n(2+3+2)
+		for (BigInteger i = new BigInteger("2"); n.divide(two).compareTo(i) > 0; i = i.add(one)) { //2+3+n/2(2+3+2)
 			if (n.mod(i).equals(zero)) {
 				return false;
 			}
@@ -103,13 +103,13 @@ public class MCD {
 		return true; //1
 	}
 
-	// Metodo que determina si un numero es primo con pasos de 2 | T(n) = 4+2+2+3+(n/2)(2+3+2)+1 = 12+7n/2
+	// Metodo que determina si un numero es primo con pasos de 2 | T(n) = 4+2+2+3+(n/4)(2+3+2)+1 = 12+7n/4
 	public static boolean isPrimo2(BigInteger n) {
 		BigInteger zero = new BigInteger("0"), two = new BigInteger("2"); //4
 		// Si es divisible por dos no es primo
 		if(n.divide(two).equals(zero)) return false; //2
 		// Comprobamos si es divisible por algun numero impar desde 3 hasta n/2
-		for (BigInteger i = new BigInteger("3"); n.divide(two).compareTo(i) > 0; i = i.add(two)) { //2+3+(n/2)(2+3+2)
+		for (BigInteger i = new BigInteger("3"); n.divide(two).compareTo(i) > 0; i = i.add(two)) { //2+3+(n/4)(2+3+2)
 			if (n.mod(i).equals(zero)) {
 				return false;
 			}
